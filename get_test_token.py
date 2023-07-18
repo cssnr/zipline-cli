f8b70b8a72976b5be32bec6d7950f6fcde1c4575
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-17 20:25:15 -0700

    Update

--- get_test_token.py
+++ get_test_token.py
@@ -4,14 +4,12 @@ import psycopg2
 import requests
 
 query = 'SELECT "token" FROM public."User" WHERE id=1 ORDER BY id ASC;'
-
 pg_data = {
     'host': 'postgres',
     'database': 'postgres',
     'user': 'postgres',
     'password': 'postgres',
 }
-
 zip_data = {
     'username': 'administrator',
     'password': 'password',
@@ -22,11 +20,11 @@ zip_data = {
 zip_uri = os.environ.get('ZIPLINE_URL', 'http://zipline:3000').rstrip('/')
 zip_url = f'{zip_uri}/api/auth/login'
 
-# log in to create account (and get cookie)
+# log in to create account
 r = requests.post(zip_url, json=zip_data)
 r.raise_for_status()
 
-# get token (or could use cookie, not tested)
+# get token from postgres
 with psycopg2.connect(**pg_data) as conn:
     with conn.cursor() as cur:
         cur.execute(query)
@@ -37,5 +35,5 @@ if not result:
     print(f'No result from postgres query: {query}')
     sys.exit(1)
 
-# export environment variable
+# print environment variable for export
 print(f'{result[0]}')

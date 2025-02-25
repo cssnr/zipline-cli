480697be529978948454037720a05d7cb5126530
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-25 14:49:05 -0800

    ...

--- get_test_token.py
+++ get_test_token.py
@@ -1,48 +1,40 @@
 import os
-import sys
 import time
 
-import psycopg2
 import requests
 
 
-pg_data = {
-    "host": "localhost",
-    "database": "postgres",
-    "user": "postgres",
-    "password": "postgres",
-}
 zip_data = {
     "username": "administrator",
     "password": "password",
     "code": None,
 }
-query = 'SELECT "token" FROM public."User";'
 
-# set zipline url
 zip_uri = os.environ.get("ZIPLINE_URL", "http://localhost:3000").rstrip("/")
+
+s = requests.Session()
+
+
 # setup
-r = requests.post(f"{zip_uri}/api/setup", json=zip_data)
+r = s.post(f"{zip_uri}/api/setup", json=zip_data)
 r.raise_for_status()
-time.sleep(3)
+# print(r.status_code)
+# print(r.json())
+time.sleep(2)
+
+
 # login
-r = requests.post(f"{zip_uri}/api/auth/login", json=zip_data)
+r = s.post(f"{zip_uri}/api/auth/login", json=zip_data)
 r.raise_for_status()
-time.sleep(3)
-r = requests.post(f"{zip_uri}/api/auth/login", json=zip_data)
+# print(r.status_code)
+# print(r.json())
+
+
+# token
+r = s.get(f"{zip_uri}/api/user/token")
 r.raise_for_status()
-time.sleep(3)
+# print(r.status_code)
+data = r.json()
+# print(data)
 
-# get token from postgres
-with psycopg2.connect(**pg_data) as conn:  # type: ignore
-    with conn.cursor() as cur:
-        cur.execute(query)
-        result = cur.fetchone()
-
-# exit with error if no results
-if not result:
-    print(f"No result from postgres query: {query}")
-    sys.exit(1)
-
-# print environment variable for export
-print(f"{result[0]}")
+print(data["token"])

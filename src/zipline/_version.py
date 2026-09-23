5f86d78e3707e48a3a8df070bdc380d662f14494
Shane <6071159+smashedr@users.noreply.github.com>
2026-09-23 15:54:18 -0700

    Add draft.yaml and Update _version.py

--- src/zipline/_version.py
+++ src/zipline/_version.py
@@ -1,12 +1,20 @@
 import os
 import re
 
+SUFFIXES = {"alpha": "a", "beta": "b", "rc": "rc"}
+
 
 def get_version() -> str:
     version = os.environ.get("GITHUB_REF_NAME", "")
-    pattern = r"^\d+\.\d+\.\d+(?:[abc]\d*)?$"
-    match = re.match(pattern, version)
-    return version if match else "0.0.1"
+    match = re.match(r"^v?(\d+\.\d+\.\d+)-(alpha|beta|rc)\.(\d+)$", version)
+    if match:
+        base, suffix, number = match.groups()
+        version = f"{base}{SUFFIXES[suffix]}{number}"
+    elif version.startswith("v"):
+        version = version[1:]
+    if re.match(r"^\d+\.\d+\.\d+(?:(?:[abc]|rc)\d*)?$", version):
+        return version
+    return "0.0.1"
 
 
 __version__ = get_version()
